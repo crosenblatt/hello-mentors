d6e201b41eb3dc671e35a4fd0f97a401dbd81ea5
crosenblatt <rosench13@gmail.com>
2019-04-11 16:02:01 -0400

    fixed table css for mentor. the css comes from https://codepen.io/AllThingsSmitty/pen/MyqmdM

--- js/mentor.js
+++ js/mentor.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".ticketTable tbody").empty();
-    $(".ticketTable tbody").append('<tr><td align = "center"> Chris </td> <td align = "center"> Help </td><td align = "center"> Vikkybums Rooms </td> <td align = "center"> <a href = "" class = "claim"> Open </a></td></tr>');
+    $(".ticketTable tbody").append('<tr><td align = "center" data-label = "Name"> Chris </td> <td align = "center" data-label = "Problem Description"> Help </td><td align = "center" data-label = "Location"> Vikkybums Rooms </td> <td align = "center" data-label="status"> <a href = "" class = "claim"> Open </a></td></tr>');
 
     $(".claim").click(function() {
         $(this).text($(this).text() == 'Open' ? 'Finish' : 'Open');
