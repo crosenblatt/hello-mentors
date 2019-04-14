9d6ce478943ca0bd59dcce9feee74f300ba31df2
Vikas Tatineni <vtatinen@purdue.edu>
2019-04-14 00:11:21 -0400

    Check in and Check out mentor frontend is done

--- js/organizer.js
+++ js/organizer.js
@@ -22,5 +22,85 @@ $(document).ready(function() {
     		}
     	});
 
-    //$(".ticketTable tbody").append('<tr><td align = "center" data-label = "Name"> Chris </td> <td align = "center" data-label = "Problem Description"> Help </td><td align = "center" data-label = "Location"> Vikkybums Rooms </td> <td align = "center" data-label="Tags"> ANDROID </td> <td align = "center" data-label="status"> <a href = "" class = "claim"> Open </a></td></tr>');
+    $("#sign-in-mentor-form").submit(function(event) {
+        $.ajax({
+            url: "/api/checkin-mentor",
+            type: "POST",
+            dataType: "json",
+            data: $("#sign-in-mentor-form").serialize(),
+            success: function(result) {
+                //console.log(result);
+                if (result.success == true) {alert("Mentor signed in");}
+                else {alert("Invalid email or Mentor already signed in");}
+            },
+            error: function(err) {
+                //console.log(err);
+            }
+        });
+        
+        $("#sign-in-mentor-form").each(function() {
+            this.reset();
+        })
+
+        /* USED TO UPDATE CURRENT MENTORS
+        $.ajax({
+            url: "/api/get-hacker-tickets",
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+                $("#hacker-tickets tbody").empty();
+                $.each(data, function(index, item){
+                    var date = new Date(item.submit_time);
+                    $("<tr>").append(
+                    $("<td align = \"center\" data-label = \"Problem\">").text(item.message),
+                    $("<td align = \"center\" data-label = \"Tags\">").text(item.tags),
+                    $("<td align = \"center\" data-label = \"Time Submitted\">").text(`${date.toLocaleTimeString('en-US', {hour:'numeric', minute:'numeric', hour12:true})}`),
+                    ).appendTo("#hacker-tickets tbody");
+                });
+            }
+        })
+        */
+        event.preventDefault();
+    });
+
+    $("#sign-out-mentor-form").submit(function(event) {
+        $.ajax({
+            url: "/api/checkout-mentor",
+            type: "POST",
+            dataType: "json",
+            data: $("#sign-out-mentor-form").serialize(),
+            success: function(result) {
+                //console.log(result);
+                if (result.success == true) {alert("Mentor signed out");}
+                else {alert("Invalid email or Mentor already signed out");}
+            },
+            error: function(err) {
+                //console.log(err);
+            }
+        });
+        
+        $("#sign-out-mentor-form").each(function() {
+            this.reset();
+        })
+
+        /* USED TO UPDATE CURRENT MENTORS
+        $.ajax({
+            url: "/api/get-hacker-tickets",
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+                $("#hacker-tickets tbody").empty();
+                $.each(data, function(index, item){
+                    var date = new Date(item.submit_time);
+                    $("<tr>").append(
+                    $("<td align = \"center\" data-label = \"Problem\">").text(item.message),
+                    $("<td align = \"center\" data-label = \"Tags\">").text(item.tags),
+                    $("<td align = \"center\" data-label = \"Time Submitted\">").text(`${date.toLocaleTimeString('en-US', {hour:'numeric', minute:'numeric', hour12:true})}`),
+                    ).appendTo("#hacker-tickets tbody");
+                });
+            }
+        })
+        */
+        event.preventDefault();
+    });
 });
