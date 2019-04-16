ca27bda2f578d1273828cebfe7ba31fae4891402
crosenblatt <rosench13@gmail.com>
2019-04-16 19:37:43 -0400

    change password page

--- js/change-password.js
+++ js/change-password.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
     $("#new-pw-form").submit(function(event) {
-        var match = $('#password').val() == $('#confirm-password').val();
-        if(!match) {
+        if($('#password').val() !== $('#confirm-password').val()) {
             alert("Passwords Do Not Match");
-            event.preventDefault();
+        } else {
+            $.ajax({
+                url: window.location.href.substring(window.location.href.indexOf("/change-password")),
+                type: "POST",
+                dataType: "json",
+                data: { password: $("#password").val() },
+                success: function(data, textStatus) {
+                    console.log(data);
+                    if(data.redirect) {
+                        window.location.href = data.redirect;
+                    } else {
+
+                    }
+                },
+
+                complete: function(data, textStatus) {
+                    console.log(data);
+                    if(data.status == 200) {
+                        window.location.search = "";
+                        window.location.pathname = "/login";
+                    } else {
+                        
+                    }
+                }
+            });
+            //alert(window.location.href.substring(window.location.href.indexOf("/change-password")));
         }
+
+        event.preventDefault();
     })
 })
